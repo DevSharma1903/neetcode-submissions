8775efdb27d7f4807ccef89bf7b8c015756d919f
Dev Sharma <devsharma.techinnovate@gmail.com>
2026-07-29 11:03:24 +0530

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] dp;
+
+    public int rob(int[] nums) {
+        dp = new int[nums.length];
+
+        Arrays.fill(dp, -1);
+
+        return test(nums, 0);
+    }
+
+    public int test(int[] nums, int i){
+        if(i >= nums.length) return 0;
+        if(dp[i] != -1) return dp[i];
+
+        // int h1 = test(nums, i+1);
+        // int h2 = nums[i] + test(nums, i+2);
+
+        return dp[i] = Math.max(test(nums, i+1), nums[i] + test(nums, i+2));
+    }
+}
