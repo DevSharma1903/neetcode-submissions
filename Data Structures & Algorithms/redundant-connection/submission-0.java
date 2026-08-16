62264eed96ebded2800214a179136934c448a1c1
Dev Sharma <devsharma.techinnovate@gmail.com>
2026-08-16 15:45:43 +0530

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.java
@@ -0,0 +1,53 @@
+class Solution {
+    int arr;
+    public int[] findRedundantConnection(int[][] edges) {
+        DSU dsu = new DSU(edges.length+1);
+        for(int[] i : edges){
+            if(!dsu.solve(i[0], i[1])){
+                return i;
+            }
+        }
+        return new int[]{};
+    }
+}
+
+class DSU{
+    List<Integer> par = new ArrayList<>();
+    List<Integer> rank = new ArrayList<>();
+
+    public DSU(int n){
+        for(int i=0; i<n; i++){
+            par.add(i);
+            rank.add(0);
+        }
+    }
+
+    public int findUlt(int node){
+        if(node == par.get(node)){
+            return node;
+        }
+
+        int ult = findUlt(par.get(node));
+        par.set(node, ult);
+        return ult;
+    }
+
+    public boolean solve(int u, int v){
+        int pu = findUlt(u);
+        int pv = findUlt(v);
+
+        if(pu == pv) return false;
+
+        if(rank.get(pu) < rank.get(pv)){
+            par.set(pu, pv);
+        }else if(rank.get(pu) > rank.get(pv)){
+            par.set(pv, pu);
+        }
+        else{
+            par.set(pv, pu);
+            rank.set(pu, rank.get(pu) + 1);
+        }
+
+        return true;
+    }
+}
